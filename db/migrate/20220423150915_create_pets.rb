ee4361555b4a541dffdd1b825af7fbb5620d7dd2
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-04-23 17:08:21 +0100

    Pets rails app

---
+++ db/migrate/20220423150915_create_pets.rb
@@ -0,0 +1,12 @@
+class CreatePets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :breed
+      t.date :found_on
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
